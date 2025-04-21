d822a0c81afcc89785fc4670ac0fb3ac7901ac46
GarciaBrian <garciabrianalejandro@gmail.com>
2025-04-21 16:34:12 -0300

    empiezo a maquetar y agrego iconos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Pokeball from '../../assets/icons/pokeball.svg'
+import Searcher from '../../assets/icons/searcher.svg'
 
 const Home = () => {
   return (
-    <div>Holaaaa</div>
+    <div>
+      <header>
+        <div>
+          <img src={Pokeball} alt="" />
+          <h1>Pokédex</h1>
+        </div>
+        <div>
+          <input 
+          type="text"
+          placeholder='Buscar'
+           />
+           <button type='submit'></button>
+        </div>
+      </header>
+      <div>
+        <div>
+          <p>3</p>
+          <img src={Pokeball} alt="" />
+          <p>pokemon name</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/PokemonDetail/PokemonDetail.jsx
+++ src/pages/PokemonDetail/PokemonDetail.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { IoMdArrowBack } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const PokemonDetail = () => {
   return (
-    <div>PokemonDetail</div>
+    <div>
+      <div>
+        <IoMdArrowBack />
+        <h2>Pokémon name</h2>
+        <span>9</span>
+      </div>
+      <div>
+        <IoIosArrowBack />
+        <div>
+          <img src="" alt="" />
+          <span>type</span>
+          <span>type</span>
+        </div>
+        <IoIosArrowForward />
+      </div>
+      <div>
+        <h4>About</h4>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Nisi, necessitatibus? Dolores dolor animi, non vitae earum 
+          veritatis ex quo, temporibus reprehenderit, qui consectetur 
+          ratione libero asperiores hic architecto perspiciatis aspernatur?
+        </p>
+        <h4>Base Stats</h4>
+        <ul>
+          <li>hp 999</li>
+          <li>hp 999</li>
+          <li>hp 999</li>
+          <li>hp 999</li>
+          <li>hp 999</li>
+          <li>hp 999</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
