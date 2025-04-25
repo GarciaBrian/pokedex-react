31fc8263de35964e13088d353ff0afe7ed9c2993
GarciaBrian <garciabrianalejandro@gmail.com>
2025-04-25 20:11:23 -0300

    hice fetch con la pokeapi y sigo estilizando

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Pokeball from '../../assets/icons/pokeball.svg'
 import './PokemonList.css'
 
-const PokemonList = () => {
+const PokemonList = ({pokemons}) => {
     return (
         <div className='container-pokemon-list'>
-            {/* <p>3</p>
-            <img src={Pokeball} alt="" />
-            <p>pokemon name</p> */}
+            <div className='container-pokemon-card'>
+                {pokemons.map((pokemon) =>(
+                    <div key={pokemon.id} className='pokemon-card'>
+                    <span>{pokemon.id}</span>
+                    <img src={pokemon.image} alt={pokemon.name} />
+                    <p>{pokemon.name}</p>
+                    </div>
+
+                ))}
+            </div>
         </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import Pokeball from '../../assets/icons/pokeball.svg'
 import Searcher from '../../assets/icons/searcher.svg'
 import PokemonList from '../../components/PokemonList/PokemonList'
@@ -8,6 +8,41 @@ import './Home.css'
 
 
 const Home = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  const baseURL = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const res = await fetch(`${baseURL}?limit=12`)
+      const data = await res.json()
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon) =>{
+          const res = await fetch(pokemon.url)
+          const details = await res.json()
+
+          return {
+            id:details.id,
+            name: details.name,
+            image: details.sprites.other.home.front_default,
+            height: details.height,
+            weight: details.weight,
+            types: details.types.map(type => type.type.name),
+            abilities: details.abilities.map(a => a.ability.name),
+            stats: details.stats.map(stat => ({
+              name: stat.stat.name,
+              value: stat.base_stat
+            })),
+          }
+        })
+      )
+
+      setPokemons(detailedPokemons)
+    }
+
+    fetchPokemons()
+  }, [])
+
   return (
     <div>
       <header className='header-home'>
@@ -18,7 +53,7 @@ const Home = () => {
         <div className='container-input'>
           <div className='container-input-div'>
             <IoIosSearch className='search-icon' />
-            <IoMdClose className='close-icon' />
+            {/* <IoMdClose className='close-icon' /> */}
             <input
               type="text"
               placeholder='Buscar'
@@ -28,7 +63,7 @@ const Home = () => {
           </button>
         </div>
       </header>
-      <PokemonList />
+      <PokemonList pokemons={pokemons}/>
     </div>
   )
 }
